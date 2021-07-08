a5e756ea4b6570a0abeb0f041b36bbdeb9bcb87e
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-08 11:37:20 +0530

    [Afrath]Updated the file with create board UC1

--- TicTacToeProblem/Program.cs
+++ TicTacToeProblem/Program.cs
@@ -7,6 +7,8 @@ namespace TicTacToeProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic Tac Toe Problem");
+            TicTacToeGame ticTacToeGame = new TicTacToeGame();
+            ticTacToeGame.createBoard();
         }
     }
 }
---
+++ TicTacToeProblem/TicTacToeGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeProblem
+{
+    
+    class TicTacToeGame
+    {
+        char[] boardArray = { '0','0', '0', '0', '0', '0', '0', '0', '0', '0' };
+        public void createBoard()
+        {
+            Console.WriteLine( boardArray[1] +" | "+boardArray[2]+ " | "+boardArray[3]);
+            Console.WriteLine("-----------");
+            Console.WriteLine(boardArray[4] + " | " + boardArray[5] + " | " + boardArray[6]);
+            Console.WriteLine("-----------");
+            Console.WriteLine(boardArray[8] + " | " + boardArray[7] + " | " + boardArray[9]);
+
+        }
+
+    }
+}
